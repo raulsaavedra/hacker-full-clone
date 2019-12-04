7ece982533f00da89ef27efdc670981c88f5ee49
TheKaizenWolf <raul670saavedra@gmail.com>
2019-12-04 17:57:52 -0600

    added a refetch every hour

--- backend/server.js
+++ backend/server.js
@@ -30,14 +30,39 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static(CLIENT_BUILD_PATH));
 
-// schedule.scheduleJob('*/1 * * * *', async () => {
+schedule.scheduleJob('*/60 * * * *', async () => {
+  const response = await fetch(
+    'https://hn.algolia.com/api/v1/search_by_date?query=nodejs'
+  );
+  const data = await response.json();
+  const posts = data.hits;
+  const todayHour = new Date();
+  todayHour.setHours(todayHour.getHours() - 1);
+  console.log(posts);
+  console.log('testing update');
+  posts.map(async post => {
+    const postDate = new Date(post.created_at);
+    console.log('post', postDate);
+    console.log('todayhour', todayHour);
+    if (postDate >= todayHour)
+      await Post.create({
+        title: post.title,
+        story_title: post.story_title,
+        author: post.author,
+        created_at: post.created_at,
+        url: post.url,
+        story_url: post.story_url,
+      });
+  });
+});
+
 app.get('/create', async (req, res) => {
   const response = await fetch(
     'https://hn.algolia.com/api/v1/search_by_date?query=nodejs'
   );
   const data = await response.json();
   const posts = data.hits;
-  posts.map(post =>
+  posts.map(async post =>
     Post.create({
       title: post.title,
       story_title: post.story_title,
@@ -60,7 +85,7 @@ app.get('/posts', async (req, res) => {
 });
 
 app.post('/delete', async (req, res) => {
-  await Post.findByIdAndDelete(req.body.id);
+  await Post.findByIdAndUpdate(req.body.id, { title: null, story_title: null });
   console.log(req.body.id);
   res.status(201).json('success');
 });
